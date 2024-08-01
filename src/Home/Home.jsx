34856cc42d97f35065e6736597062576908ab751
Noelisoa Alain <alainnoelisoa@gmail.com>
2024-08-01 16:22:36 +0300

    Navbar Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,10 +3,22 @@ import LeftBar from './NavBar/LeftBar'
 import NavBar  from './NavBar/NavBar'
 import "./home.css"
 
+// Icon
+import Profil from './HomeAssets/person.png'
+import Notification from './HomeAssets/notification-bell.png'
+import Search from './HomeAssets/search-interface-symbol.png'
+import Logo from './HomeAssets/burn.png'
+
 function Home() {
   return (
     <div className='Home'>
-        <NavBar></NavBar>
+        <NavBar 
+          profil={Profil} 
+          notification={Notification}
+          search = {Search}
+          logo= {Logo}
+        ></NavBar>
+
         <LeftBar></LeftBar>
     </div>
   )
--- src/Home/NavBar/NavBar.jsx
+++ src/Home/NavBar/NavBar.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 
-function NavBar() {
+function NavBar({profil,notification,search,logo}) {
   return (
     
     <div className='Navbar bg-dark text-primary d-flex align-items-center'>
-      <div className='logo cercle'>
-
-      </div>
       
-      <div className="logo_Search">
+      {/* Logo and Search Bar */}
+      <div className="logo_search d-flex align-items-center">
+        
+        {/* Logo */}
+        <div className='logo cercle d-flex align-items-center justify-content-center'>
+          <img src={logo} alt="" className='w-75'/>
+        </div>
+
+        {/* Search Bar */}
         <form action="">
-            <input type="search" name="" id="" className='search form-control'/>
+          <div className='searchBar d-flex align-items-center'>
+            <img src={search} alt="" className='searchIcon w-20' />
+            <input type="search" name="" className='search form-control'/>
+          </div>
         </form>
+
       </div>
 
+      {/* Profil and Notification */} 
       <div className='p_n d-flex'>
-        <div className="profil cercle">
-            P
+
+        <div className="notification cercle d-flex align-items-center justify-content-center">
+            <img src={notification} alt="" className='w-50'/>
         </div>
-        <div className="notification cercle">
-            N
+
+        <div className="profil cercle d-flex align-items-center justify-content-center">
+            <img src={profil} alt="" className='w-75'/>
         </div>
+
       </div>
 
     </div>
