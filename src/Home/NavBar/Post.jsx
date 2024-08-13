91e158b21d8d60fce15c5a575049a30fe8889df4
Noelisoa Alain <alainrakotoarivony926@gmail.com>
2024-08-13 23:53:17 +0300

    Publication

--- src/Home/NavBar/Post.jsx
+++ src/Home/NavBar/Post.jsx
@@ -6,11 +6,21 @@ import pubExemple from '../PublicationCard/PostAsset/pubEx.jpg'
 function Post() {
   return (
     <div className='containerPost d-flex justify-content-center '>
-        <div className='text-primary post bg-dark overflow-auto'>
-          <div className='containerpost ' >
+        <div className='post bg-dark'>
+          <div className='containerpost' >
             {/* All pub */}
             <PublicationCard title={"test title"} picture={pubExemple} description={"Asvbsjb ew w eh sn  dfisfhe"}/>
-            <PublicationCard title={"test title"} picture={pubExemple} description={"Asvbsjb ew w eh sn  dfisfhe"}/>
+            <PublicationCard title={"test title"} picture={pubExemple} 
+                description={"Asvbsjb ew wfd gf rt rrt tj ytjt jtyrt jrtjrjrtr eh sn"+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
+                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "
+                }
+            />
           </div>
 
         </div>
--- src/Home/PublicationCard/PublicationCard.jsx
+++ src/Home/PublicationCard/PublicationCard.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-function PublicationCard({title,picture,description}) {
+function PublicationCard({ title, picture, description }) {
+  const Description = useRef(null)
+  const [height, setHeight] = useState(0)
+  useEffect(() => {
+    // Vérifie si la référence est définie
+    if (Description.current) {
+      // Récupère la hauteur du div
+      const divHeight = Description.current.clientHeight;
+      setHeight(divHeight);
+    }
+  }, []);
   return (
-    <div className='publication bg-white text-left'>
-        <h1>title :{title}</h1>
-        <img src={picture} alt="" className='w-100'/>
-        <div className="description">
-            <p>{description}</p>
+    <div className='publication text-white'>
+      <h2>title :{title}{height}</h2>
+      <div className="description" style={{ position: "relative", textAlign: "justify", height: "24px" }} >
+        {/* <p className='textDescription'></p> */}
+        <p ref={Description}>{description}</p>
+        <div className='flouSpan' style={{ position: "absolute", right: "0", top: "0", zIndex:"2"}}>
+          <span className="" style={{marginLeft:"50px"}}>see more . . .</span>
         </div>
+      </div>
+      <img src={picture} alt="" className='imagepub w-100' />
+
     </div>
   )
 }
