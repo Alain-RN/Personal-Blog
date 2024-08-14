881fb4f9caf74d6d835293dfb2bdceb5c41edde4
Noelisoa Alain <alainrakotoarivony926@gmail.com>
2024-08-14 23:54:28 +0300

    More pub

--- src/Home/NavBar/Post.jsx
+++ src/Home/NavBar/Post.jsx
@@ -6,24 +6,31 @@ import pubExemple from '../PublicationCard/PostAsset/pubEx.jpg'
 function Post() {
   return (
     <div className='containerPost d-flex justify-content-center '>
-        <div className='post bg-dark'>
-          <div className='containerpost' >
-            {/* All pub */}
-            <PublicationCard title={"test title"} picture={pubExemple} description={"Asvbsjb ew w eh sn  dfisfhe"}/>
-            <PublicationCard title={"test title"} picture={pubExemple} 
-                description={"Asvbsjb ew wfd gf rt rrt tj ytjt jtyrt jrtjrjrtr eh sn"+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "+
-                  "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "
-                }
-            />
-          </div>
-
+      <div className='post bg-dark'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam libero voluptate aperiam veritatis minus tenetur id sequi, maiores mollitia esse.
+        <div className='containerpost' >
+          {/* All pub */}
+          <PublicationCard title={"test title"} picture={pubExemple} description={"Asvbsjb ew w eh sn  dfisfhe"} />
+          <PublicationCard title={"test title"} picture={pubExemple}
+            description={"Asvbsjb ew wfd gf rt rrt tj ytjt jtyrt jrtjrjrtr eh sn" +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf " +
+              "dfisfhefsbrwhr  34y er dfhd hdfhdf  h fdh rthr htrh rth rt hrthfgbcbf "
+            }
+          />
+          <PublicationCard title={"test title"} picture={pubExemple}
+            description={`Lorem ipsum dolor sit amet consectetur adipisicing el
+              it. Ipsam libero voluptate aperiam veritatis minus tenetur id sequi, m
+              aiores mollitia esse.`
+            }
+          />
         </div>
+
+      </div>
     </div>
 
   )
